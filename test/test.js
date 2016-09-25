765de32b8a62eb60000056c6c4137eaeeae7732b
IsuruDilhan <inbox.isuru@gmail.com>
2016-09-25 20:22:26 +0530

    Replace App

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@
 'use strict';
 
 var request = require("supertest"),
-    app = require("./../app");
+    app = require("../app").getApp;
 
 describe('GET /', function(){
-    it('expects HTTP response 200', function(done){
-        request(app)
-            .get('/')
-            .expect(200, done);
-    });
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
 });
